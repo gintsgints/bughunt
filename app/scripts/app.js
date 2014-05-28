d2697f3f9425d133097675ce8183709aa53e0cf3
gintsgints <polis.gints@gmail.com>
2014-05-28 23:25:54 +0300

    ui-route instead of ng-route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,31 @@
 
 angular
   .module('bughuntApp', [
+    'ui.router',
     'ngCookies',
     'ngResource',
     'ngSanitize'
-  ]);
+  ])
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('menu', {
+      templateUrl: 'views/menu.html',
+    })
+    .state('menu.main', {
+      url: '/',
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .state('menu.about', {
+      url: '/about',
+      templateUrl: 'views/about.html',
+      controller: 'MainCtrl'
+    })
+  ;
+})
+
+;

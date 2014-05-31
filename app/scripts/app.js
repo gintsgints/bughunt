193d04283612aedd87775fae744b43e5bc1153b4
gintsgints <polis.gints@gmail.com>
2014-05-31 18:28:16 +0300

    Update layout so angular build not complain.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,23 +13,27 @@ angular
 
 .config(function ($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/');
 
-  $stateProvider
-    .state('menu', {
-      templateUrl: 'views/menu.html',
-    })
-    .state('menu.main', {
-      url: '/',
-      templateUrl: 'views/main.html',
-      controller: 'MainCtrl'
-    })
-    .state('menu.about', {
-      url: '/about',
-      templateUrl: 'views/about.html',
-      controller: 'MainCtrl'
-    })
-  ;
-})
-
+    $stateProvider
+      .state('menu', {
+        templateUrl: 'views/menu.html',
+      })
+      .state('menu.main', {
+        url: '/',
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .state('menu.bug', {
+        url: '/bug',
+        templateUrl: 'views/bug_edit.html',
+        controller: 'MainCtrl'
+      })
+      .state('menu.about', {
+        url: '/about',
+        templateUrl: 'views/about.html',
+        controller: 'MainCtrl'
+      })
+    ;
+  })
 ;

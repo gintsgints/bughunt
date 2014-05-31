f7e45760f83f23b40982d256ff840a88bad4c666
gintsgints <polis.gints@gmail.com>
2014-05-31 18:33:07 +0300

    So no complains about global variable Firebase.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/*global Firebase */
+
 angular.module('bughuntApp')
   .controller('MainCtrl', function ($scope, $firebase, FIREBASE_URL) {
     var bugsref = new Firebase(FIREBASE_URL + 'bugs');
